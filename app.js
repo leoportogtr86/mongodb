6920efc0ca2ab8c76f98602b9706133e8cf4442c
Leo Porto <leoporto_studio@hotmail.com>
2020-09-23 17:19:21 -0300

    criacao do primeiro model/collection e salvando os dados de uma instancia

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
-
+//criacao do model (classe/collection) Pessoa
 const Pessoa = mongoose.model('Pessoa', {
 
     nome: String,
@@ -9,8 +9,12 @@ const Pessoa = mongoose.model('Pessoa', {
 
 })
 
+
+//uma instancia do modelo pessoa
 let pessoa = new Pessoa({ nome: 'Davi Porto', idade: 4 })
 
+
+//salvando os dados
 pessoa.save().then(() => {
 
     console.log('dados salvos')
