ce1882e86ff8b5d75340d1039234eb8b0f3cb6e9
Leo Porto <leoporto_studio@hotmail.com>
2020-09-24 07:53:29 -0300

    selecionando os campos que devem ou nao ser mostrados na busca

--- comandos_basicos/buscar_documentos.js
+++ comandos_basicos/buscar_documentos.js
@@ -12,3 +12,8 @@ db.test.findOne()
 
 //refinando a busca, neste caso, buscando por tipo de instrumento
 db.instrumento.find({ tipo: "cordas" })
+
+
+//selecionando o que deve ser mostrado na busca, 0 => nao mostra o campo, 1 => mostra o campo
+db.instrumento.find({ tipo: "percussao" }, { tipo: 0 })
+db.instrumento.find({ tipo: "cordas" }, { nome: 0 })
