f08a2a3abf4578c3876dfdbbe3ee3c8906ef0da5
Leo Porto <leoporto_studio@hotmail.com>
2020-09-24 07:49:00 -0300

    refinando a busca, busca por tipo de instrumento

--- comandos_basicos/buscar_documentos.js
+++ comandos_basicos/buscar_documentos.js
@@ -7,3 +7,8 @@ db.test.find().pretty()
 
 //buscando o primeiro registro
 db.test.findOne()
+
+
+
+//refinando a busca, neste caso, buscando por tipo de instrumento
+db.instrumento.find({ tipo: "cordas" })
