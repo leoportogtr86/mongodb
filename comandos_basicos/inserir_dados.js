7879ee11fa5bdd1360352816720cd2fd043c408a
Leo Porto <leoporto_studio@hotmail.com>
2020-09-24 08:00:30 -0300

    inserindo varios registros usando array

--- comandos_basicos/inserir_dados.js
+++ comandos_basicos/inserir_dados.js
@@ -1,1 +1,5 @@
 db.test.insert({ nome: "Júlio", idade: 78 })
+
+
+//inserindo varios dados
+db.test.insert([{ nome: "Mônica", idade: 10 }, { nome: "Cascão", idade: 10 }, { nome: "Cebolinha", idade: 10 }, { nome: "Magali", idade: 10 }])
