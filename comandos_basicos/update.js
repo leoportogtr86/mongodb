9e172f5d4efa3c12ee604435928380f5b915989d
Leo Porto <leoporto_studio@hotmail.com>
2020-09-24 08:20:42 -0300

    atualizando multiplos registros

--- comandos_basicos/update.js
+++ comandos_basicos/update.js
@@ -9,4 +9,5 @@ db.test.update({ nome: "Cebolinha" }, { nome: "Cebola", idade: 11 })
 db.test.update({ nome: "Cebola" }, { $set: { nome: "Cebolão" } })
 
 //atualizando multiplos registros que atendam uma condicao. Usamos o "multi: 1 (ou true)"
+//por padrao, o mongo so altera o primeiro registro que atender certa condicao
 db.test.update({ nome: "Leo" }, { $set: { nome: "Leonardo" } }, { multi: 1 })
